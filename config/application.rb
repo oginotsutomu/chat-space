7fff1a9acea6da5334d9d0f8fd0676ca001068c0
oginotsutomu <oginotsutomugigi@icloud.com>
2019-12-12 10:14:57 +0900

    更新

--- config/application.rb
+++ config/application.rb
@@ -8,12 +8,15 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
-      config.generators do |g|
-        g.stylesheets false
-        g.javascripts false
-        g.helper false
-        g.test_framework false
-        config.i18n.default_locale = :ja
-      end
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
     end
+    config.i18n.default_locale = :ja
+    config.action_view.field_error_proc = Proc.new do |html_tag, instance| 
+      html_tag
+    end
+  end
 end
